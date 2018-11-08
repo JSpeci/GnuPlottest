b43fbb1ca5d44a55a568ba83777d04a4c438cf4c
Your Name <you@example.com>
2018-11-08 23:28:18 +0100

    changed to save pdf

--- GnuPlotTestDotNet/Program.cs
+++ GnuPlotTestDotNet/Program.cs
@@ -19,19 +19,25 @@ namespace GnuPlotTest
             }
 
             // values restricted to one day 1.4.2018
-            var times = reader.values.Where(i => i.Key.Day == 1 && i.Key.Month == 4).Select(i => double.Parse(i.Key.Hour.ToString())).ToArray(); // 8644 values for each 10 seconds
+            var hours = reader.values.Where(i => i.Key.Day == 1 && i.Key.Month == 4).Select(i => double.Parse(i.Key.Hour.ToString())).ToArray(); // 8644 values for each 10 seconds
             var values = reader.values.Where(i => i.Key.Day == 1 && i.Key.Month == 4).Select(i => double.Parse(i.Value)).ToArray(); // 8644 values for each 10 seconds
 
-            double[] X = new double[] { -10, -8.5, -2, 1, 6, 9, 10, 14, 15, 19 };
-            double[] Y = new double[] { -4, 6.5, -2, 3, -8, -5, 11, 4, -5, 10 };
             string tempfolder = System.IO.Path.GetTempPath();
 
             GnuPlot.Set("xrange  [0:24]");
             GnuPlot.Set("xtics 1");
             GnuPlot.Set("ytics 500");
             GnuPlot.Set("grid");
-            GnuPlot.SaveData(times, values, tempfolder + "plot1.data");
-            GnuPlot.Plot(tempfolder + "plot1.data", "with linespoints pt " + (int)PointStyles.SolidDiamond);
+            GnuPlot.Set("title \"1.4.2018 řada P-avg-3P-C\"");
+            GnuPlot.Set("ylabel  \"P(W)\"");
+            GnuPlot.Set("xlabel  \"dayhours (h)\"");
+            GnuPlot.Set("terminal pdf size 10in,8in");
+            GnuPlot.Set("output \"P_avg_3P_C_fromGnuPlot.pdf\"");
+
+            GnuPlot.SaveData(hours, values, tempfolder + "plot1.data");
+            GnuPlot.Plot(tempfolder + "plot1.data", "with linespoints pt " + (int)PointStyles.SolidDiamond + " lt 2 lw 2");
+
+            //Console.ReadLine();
         }
     }
 }
